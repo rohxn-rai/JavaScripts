e508e5b212608978c6e9d383bc1a9fd112034a2b
rohxn-rai <rohan.rai932@outlook.com>
2024-12-09 06:25:42 -0500

    coding challenge 1

---
+++ 002/script.js
@@ -0,0 +1,29 @@
+// BMI calculator
+
+// Data 1
+let massMark = 78;
+let heightMark = 1.69;
+let massJohn = 92;
+let heightJohn = 1.95;
+
+let BMIMark = massMark / (heightMark ** 2);
+let BMIJohn = massJohn / (heightJohn ** 2);
+console.log("BMI of Mark: " + BMIMark);
+console.log("BMI of John: " + BMIJohn);
+
+let markHigherBMI = BMIMark>BMIJohn;
+console.log(markHigherBMI);
+
+// Data 2
+massMark = 95;
+heightMark = 1.88;
+massJohn = 85;
+heightJohn = 1.76;
+
+BMIMark = massMark / (heightMark ** 2);
+BMIJohn = massJohn / (heightJohn ** 2);
+console.log("BMI of Mark: " + BMIMark);
+console.log("BMI of John: " + BMIJohn);
+
+markHigherBMI = BMIMark>BMIJohn;
+console.log(markHigherBMI);
